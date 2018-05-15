2b65ddf78c56c4e38bbdc0b5825e5605fea5590d
Joseph Poirier <jdpoirier@gmail.com>
2018-05-14 23:39:33 -0500

    remove write to console

--- monitor.cs
+++ monitor.cs
@@ -21,7 +21,6 @@ namespace ProcessSample
             Process _process = new Process();
             _process.StartInfo.FileName = args[0];  // miner path and name, e.g. c:/eth/miner/ethminer.exe
             _process.StartInfo.Arguments = args[2]; // miner options, must be enclosed in quotes
-            
 
             Console.WriteLine("----- monitor starting with options:");
             Console.WriteLine("{0}", args[2]);
@@ -34,7 +33,6 @@ namespace ProcessSample
                 while (!_process.HasExited && _process.Responding)
                 {
                     _process.Refresh();
-                    Console.WriteLine();
                     Console.WriteLine("--- Total process time: {0}", _process.TotalProcessorTime);
 
                     Thread.Sleep(minutes * 60 * 1000);
